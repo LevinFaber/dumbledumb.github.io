e17b43b77aa24e4edd7c30331135d00b7180c600
Levin Paul Jakob Faber <LFA@timetoact.group>
2017-09-25 16:15:06 +0200

    First Draft (welcome area)

---
+++ js/script.ts
@@ -0,0 +1,11 @@
+function hover(element){
+    element.fadeOut
+    element.setAttribute('src', '/assets/qrcode.png');
+    element.classList.remove('bild');
+    element.classList.add('qr');
+}
+function unhover(element){
+    element.setAttribute('src', '/assets/dumbledumb.jpg');
+    element.classList.remove('qr');
+    element.classList.add('bild');
+}
