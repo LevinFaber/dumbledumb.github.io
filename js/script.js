f24a710a6fdf85098692617a14cc5328179b91e0
Levin Paul Jakob Faber <LFA@timetoact.group>
2017-09-27 10:17:36 +0200

    CSS improvements, Beautify

--- js/script.js
+++ js/script.js
@@ -1,65 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    //QR CODE
+    var qr = false;
+    $('#me').click(function () {
+        event.preventDefault();
+        $(this).toggleClass('bild');
+        $(this).toggleClass('qr');
+        if (qr === false) {
+            $(this).attr('src', './assets/qrcode.png');
+            qr = true;
+        } else {
+            $(this).attr('src', './assets/dumbledumb.jpg');
+            qr = false;
+        }
+    //QR CODE
 
-var qr = false;
-$('#me').click(function(){   
-    event.preventDefault();
-    $(this).toggleClass('bild');
-    $(this).toggleClass('qr');
-    if(qr === false){
-        $(this).attr('src', './assets/qrcode.png');
-        qr = true;
+    });
+    //CHANNEL FRAMES
+    var activeIframe = "preview";
+    $("#youtubeC").click(function () {
+        $(activeIframe).toggle(100);
+        $("#youtube").toggle(180);
+        activeIframe = "#youtube"
+    });
+    $("#steamC").click(function () {
+        $(activeIframe).toggle(100);
+        $("#steam").toggle(180);
+        activeIframe = "#steam"
+    });
+    $("#whatsappC").click(function () {
+        $(activeIframe).toggle(100);
+        $("#whatsapp").toggle(180);
+        activeIframe = "#whatsapp"
+    });
+    $("#gmailC").click(function () {
+        $(activeIframe).toggle(100);
+        $("#gmail").toggle(180);
+        activeIframe = "#gmail"
+    });
+    //CHANNEL FRAMES
+
+
+    //SCROLL ARROWS
+    var $root = $('html, body');
+    $('#socialspacer').on('click', function () {
+        $root.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
+
+    function getpos() {
+        var pos = ($('#socialspacer').offset().top - $(window).scrollTop());
+        if (pos < 200) {
+            $('#socialspacer').attr('href', '#home');
+            $("#arrow").attr('class', 'fa fa-angle-double-up fa-3x');
+        } else {
+            $('#socialspacer').attr('href', '#socialspacer');
+            $("#arrow").attr('class', 'fa fa-angle-double-down fa-3x');
+        }
     }
-    else{
-        $(this).attr('src', './assets/dumbledumb.jpg');
-        qr = false;
-    }
+    getpos();
+    $(window).scroll(getpos);
+    //SCROLL ARROWS
 
-});
-  
-var activeIframe = "preview";
-$("#youtubeC").click(function() {
-    $(activeIframe).toggle(100);
-    $("#youtube").toggle(180);
-    activeIframe = "#youtube"
-});
-$("#steamC").click(function() {
-    $(activeIframe).toggle(100);
-    $("#steam").toggle(180);
-    activeIframe = "#steam"
-});
-$("#whatsappC").click(function() {
-    $(activeIframe).toggle(100);
-    $("#whatsapp").toggle(180);
-    activeIframe = "#whatsapp"
-});
-$("#gmailC").click(function() {
-    $(activeIframe).toggle(100);
-    $("#gmail").toggle(180);
-    activeIframe = "#gmail"
-});
-
-var $root = $('html, body');
-$('#socialspacer').on('click',function() {
-    $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-}); 
-
-function getpos (){
-    var pos = ($('#socialspacer').offset().top - $(window).scrollTop());
-    if(pos < 200){
-        $('#socialspacer').attr('href', '#home');
-        $("#arrow").attr('class', 'fa fa-angle-double-up fa-3x');
-    }
-    else{
-        $('#socialspacer').attr('href', '#socialspacer');
-        $("#arrow").attr('class', 'fa fa-angle-double-down fa-3x');
-    }
-}
-getpos();
-$(window).scroll(getpos);
-
-$('#waform').validate( );
-   
 });
