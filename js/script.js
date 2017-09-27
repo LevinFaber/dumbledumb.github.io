feeb37f35e21fd08d7859453e6e5f395de06c2a3
Levin Paul Jakob Faber <LFA@timetoact.group>
2017-09-27 10:04:31 +0200

    Turning Arrow

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 
-$('#me').hover(function(){
-    $(this).attr('src', './assets/qrcode.png');
+var qr = false;
+$('#me').click(function(){   
+    event.preventDefault();
     $(this).toggleClass('bild');
     $(this).toggleClass('qr');
-},function(){
-    $(this).attr('src', './assets/dumbledumb.jpg');
-    $(this).toggleClass('bild');
-    $(this).toggleClass('qr');
+    if(qr === false){
+        $(this).attr('src', './assets/qrcode.png');
+        qr = true;
+    }
+    else{
+        $(this).attr('src', './assets/dumbledumb.jpg');
+        qr = false;
+    }
+
 });
   
 var activeIframe = "preview";
@@ -31,6 +37,28 @@ $("#gmailC").click(function() {
     $("#gmail").toggle(180);
     activeIframe = "#gmail"
 });
+
+var $root = $('html, body');
+$('#socialspacer').on('click',function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+}); 
+
+function getpos (){
+    var pos = ($('#socialspacer').offset().top - $(window).scrollTop());
+    if(pos < 200){
+        $('#socialspacer').attr('href', '#home');
+        $("#arrow").attr('class', 'fa fa-angle-double-up fa-3x');
+    }
+    else{
+        $('#socialspacer').attr('href', '#socialspacer');
+        $("#arrow").attr('class', 'fa fa-angle-double-down fa-3x');
+    }
+}
+getpos();
+$(window).scroll(getpos);
 
 $('#waform').validate( );
    
