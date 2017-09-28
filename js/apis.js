678482137527f0b86509c7c1ec78ff1108e0e843
Levin Paul Jakob Faber <LFA@timetoact.group>
2017-09-28 16:18:43 +0200

    test

--- js/apis.js
+++ js/apis.js
@@ -15,11 +15,15 @@ $(document).ready(function (){
     }
 
     var vac = new httpClient();
+    function getem(){
     vac.get("http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=AAD42D5DE55845AFF3E16ED82A426E36&steamids=76561198077704323", 
     function(response){
         var varResponse = JSON.parse(response);
         console.log(varResponse);
     });
+};
+    getem();
+    
 
 
 
